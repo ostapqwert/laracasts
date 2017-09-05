b60140446cc752483073a9f21a300fc42853d43f
Остап Бендер <ostapbender@MacBook.local>
2017-09-05 12:12:01 +0400

    added option greeting

--- terminalApp/src/SayHelloCommand.php
+++ terminalApp/src/SayHelloCommand.php
@@ -4,6 +4,7 @@ namespace Acme;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SayHelloCommand extends Command {
@@ -12,12 +13,13 @@ class SayHelloCommand extends Command {
     {
         $this->setName('sayHello')
             ->setDescription('Say hello to any')
-            ->addArgument('name', InputArgument::REQUIRED, 'Your name');
+            ->addArgument('name', InputArgument::REQUIRED, 'Your name')
+        ->addOption('greeting', null, InputOption::VALUE_OPTIONAL, 'default greeting', 'Hello Default');
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
-       $message = 'Hello '. $input->getArgument('name');
+       $message = sprintf('%s, %s', $input->getOption('greeting'), $input->getArgument('name'));
 
        $output->writeln($message);
     }

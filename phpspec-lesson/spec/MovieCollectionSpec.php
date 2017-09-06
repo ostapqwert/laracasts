a9e364a0fbaae6a01383c0dea1e8a1d58a5cbadd
Остап Бендер <ostapbender@MacBook.local>
2017-09-06 19:32:26 +0400

    blog installed

--- phpspec-lesson/spec/MovieCollectionSpec.php
+++ phpspec-lesson/spec/MovieCollectionSpec.php
@@ -8,14 +8,14 @@ use Prophecy\Argument;
 
 class MovieCollectionSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_is_initializable() // проверяет создан ли класс
     {
         $this->shouldHaveType(MovieCollection::class);
     }
 
     function it_store_a_collection_of_movie(MovieCollection\Movie $movie)
     {
-        $this->add($movie);
+        $this->add($movie); // проверяет если метод add
 
         $this->shouldHaveCount(1);
     }

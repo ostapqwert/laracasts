8fcc771af967ac70c5b472a3109d1301ee9af8d6
Alexander <alexander@MacBook.local>
2017-09-19 00:58:28 +0400

    to igor

--- blog/app/Repositories/PostsRepository.php
+++ blog/app/Repositories/PostsRepository.php
@@ -10,7 +10,8 @@ class PostsRepository
 
     public function all()
     {
-        return Post::all();
+        //return Post::all();
+        return Post::latest()->filter(request()->all(['month', 'year']))->get();
     }
 
 }
